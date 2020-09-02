b35689fcb5b00911f758a275f375449bfc02fc1f
Kofi Adu-Gyan <44382721+adugyan@users.noreply.github.com>
2020-09-02 00:19:08 -0500

    Create Average.py

---
+++ Arrays/Average.py
@@ -0,0 +1,25 @@
+#With a for function
+
+def makeList():
+  values = []
+  #n = int(input())
+  for i in range(4): #or n for user input
+    newValue = float(input())
+    values.append(newValue)
+  print(float(sum(values)/len(values)))
+
+makeList()
+  
+#With a while function 
+def getAverage():
+  values = []
+  print("Enter Integers. When you're finished enter 'done' and the program will calculate the average")
+  while True:
+    n = (input())
+    if n == "done": break
+    nums = float(n)
+    values.append(n)
+  
+  print(sum(values)/len(values))
+      
+getAverage()
