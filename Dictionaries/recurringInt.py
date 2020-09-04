83564dd7136f9df5fe3a2c30b0bdfffc8ce48d1d
Kofi Adu-Gyan <44382721+adugyan@users.noreply.github.com>
2020-09-04 13:23:06 -0500

    Create recurringInt.py

---
+++ Dictionaries/recurringInt.py
@@ -0,0 +1,21 @@
+'''
+1. Create an empty dictionary
+2. loop through the list
+3. Use the iterative variable with the empty dictionary and the get method to increment
+4. If a value count == 2, print the double
+n) '''
+lsts = [2,5,1,2,3,5,1,2,4]
+wrong = [2,4,6,3]
+
+def findDouble(arrs):
+  repeats = dict()
+  for arr in arrs:
+    repeats[arr] = repeats.get(arr, 0) + 1
+    if repeats[arr] == 2: break
+    print(arr)
+    
+    
+
+findDouble(lsts)
+
+#0(n)
